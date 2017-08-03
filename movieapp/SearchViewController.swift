4765fcd274ab84bb05956fd97b1bf573f4a32266
Richard Pap <pap.richi@gmail.com>
2017-08-04 01:00:17 +0200

    Updating search

--- movieapp/SearchViewController.swift
+++ movieapp/SearchViewController.swift
@@ -11,87 +11,79 @@ import UIKit
 class SearchViewController: UITableViewController, UISearchResultsUpdating {
     
     private let searchPresenter = SearchPresenter(service: SearchService.getInstance())
-    
-    var IS_DATA_LOADED = false
-    var searchData = [SearchListData]()
-    var searchController = UISearchController()
-    var dataListController = UITableViewController()
+    private var IS_DATA_LOADED = false
+    private var searchData = [SearchListData]()
+    private var searchController = UISearchController(searchResultsController: nil)
+    private var dataListController = UITableViewController()
     
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        
         setNavigationBarItems()
         setSearchController()
-        
         tableView.register(SearchListViewCell.self, forCellReuseIdentifier: "SearchCell")
         tableView.rowHeight = 180
         //tableView.isHidden = true
-        
         setPresenter()
-        
-        
-        
-        
-
-        // Uncomment the following line to preserve selection between presentations
-        // self.clearsSelectionOnViewWillAppear = false
-
-        // Uncomment the following line to display an Edit button in the navigation bar for this view controller.
-        // self.navigationItem.rightBarButtonItem = self.editButtonItem()
     }
+    
     
     func updateSearchResults(for searchController: UISearchController) {
         //update results tableview here
         let keywords = self.searchController.searchBar.text
         
         if let keywords = keywords {
-            searchPresenter.getDatalist(keywords)
+            if keywords.characters.count > 3 {
+                searchPresenter.getDatalist(keywords)
+            }
         }
     }
     
+    
     func setSearchController() {
-        //self.dataListController.tableView.dataSource = self
-        //self.dataListController.tableView.delegate = self
-        self.searchController.searchBar.returnKeyType = UIReturnKeyType.done
-        self.searchController = UISearchController(searchResultsController: self.dataListController)
-        self.tableView.tableHeaderView = self.searchController.searchBar
-        self.searchController.searchResultsUpdater = self
+        //dataListController.tableView.dataSource = self
+        //dataListController.tableView.delegate = self
+        //self.searchController.searchBar.returnKeyType = UIReturnKeyType.done
+        //searchController = UISearchController(searchResultsController: self.dataListController)
+        
+        searchController.searchResultsUpdater = self
+        searchController.dimsBackgroundDuringPresentation = false
+        tableView.tableHeaderView = searchController.searchBar
+        definesPresentationContext = true
     }
+    
     
     func setPresenter() {
         searchPresenter.attachView(view: self)
-        searchPresenter.getDatalist("matrix")
+        //searchPresenter.getDatalist("")
     }
+    
     
     func setNavigationBarItems() {
         self.navigationItem.title = "Search"
     }
 
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
     }
 
-    // MARK: - Table view data source
-
+    
     override func numberOfSections(in tableView: UITableView) -> Int {
-        // #warning Incomplete implementation, return the number of sections
         return 1
     }
 
+    
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        // #warning Incomplete implementation, return the number of rows
-        return 20
+        return searchData.count
     }
     
     
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        
         let cell: SearchListViewCell = tableView.dequeueReusableCell(withIdentifier: "SearchCell", for: indexPath ) as! SearchListViewCell
+        let index = indexPath.row
         
-        if IS_DATA_LOADED {
-            let index = indexPath.row
+        if IS_DATA_LOADED && searchData.count > 0 && index > 0 {
             cell.bind(searchData, index)
         }
         
@@ -99,68 +91,19 @@ class SearchViewController: UITableViewController, UISearchResultsUpdating {
     }
     
     
-    func searchBarSearchButtonClicked(_ searchBar: UISearchBar) {
-        let keywords = searchBar.text
-        keywords?.replacingOccurrences(of: " ", with: "+")
-        
-        if let keywords = keywords {
-            
-        }
-    }
-    
-    
     func setData(_ list: [SearchListData]) {
+        self.dataListController.tableView.dataSource = nil
+        self.dataListController.tableView.delegate = nil
+        
         searchData = list
         IS_DATA_LOADED = true
         tableView.reloadData()
         hideLoading()
+        
+        print("Reloading the view \(IS_DATA_LOADED)")
         //tableView.isHidden = false
     }
 
-    /*
-    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        let cell = tableView.dequeueReusableCell(withIdentifier: "reuseIdentifier", for: indexPath)
-
-        // Configure the cell...
-
-        return cell
-    }
-    */
-
-    /*
-    // Override to support conditional editing of the table view.
-    override func tableView(_ tableView: UITableView, canEditRowAt indexPath: IndexPath) -> Bool {
-        // Return false if you do not want the specified item to be editable.
-        return true
-    }
-    */
-
-    /*
-    // Override to support editing the table view.
-    override func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCellEditingStyle, forRowAt indexPath: IndexPath) {
-        if editingStyle == .delete {
-            // Delete the row from the data source
-            tableView.deleteRows(at: [indexPath], with: .fade)
-        } else if editingStyle == .insert {
-            // Create a new instance of the appropriate class, insert it into the array, and add a new row to the table view
-        }    
-    }
-    */
-
-    /*
-    // Override to support rearranging the table view.
-    override func tableView(_ tableView: UITableView, moveRowAt fromIndexPath: IndexPath, to: IndexPath) {
-
-    }
-    */
-
-    /*
-    // Override to support conditional rearranging of the table view.
-    override func tableView(_ tableView: UITableView, canMoveRowAt indexPath: IndexPath) -> Bool {
-        // Return false if you do not want the item to be re-orderable.
-        return true
-    }
-    */
 
     /*
     // MARK: - Navigation
